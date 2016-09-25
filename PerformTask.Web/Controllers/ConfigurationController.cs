5eeebb68322ee94bd2a53a18e331d74f44acab9b
PawelPodkowka <pawel.podkowka@gmail.com>
2016-09-25 19:10:20 +0200

    Prepare to gathering API address

---
+++ PerformTask.Web/Controllers/ConfigurationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace PerformTask.Web.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        private const string GRAPH_API_ADDRESS = "GraphApiAddress";
+
+        // GET: Configuration
+        public string Index()
+        {
+            return WebConfigurationManager.AppSettings[GRAPH_API_ADDRESS];
+        }
+    }
+}
--- PerformTask.Web/Scripts/app/graphService.js
+++ PerformTask.Web/Scripts/app/graphService.js
@@ -1,4 +1,14 @@
-﻿app.service('GraphService', function($http) {
+﻿app.service('GraphService', function($http, ApiConfig) {
+
+    var apiAddress = "http://localhost:54718";
+
+     //ApiConfig().success(function (result) {
+     //               apiAddress = result;
+     //           })
+     //           .error(function (reason) {
+     //               apiAddress = "http://localhost:54718";
+     //               console.error('Uuups :( I can not find best path ' + reason);
+     //           });
 
     this.getNodes = function() {
         return $http.get("http://localhost:54718/api/nodes");
